b264af1322cb7037b3a496b47add49d1eae277fb
utkarshrobin <128771305+utkarshrobin@users.noreply.github.com>
2023-05-05 17:01:32 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -20,14 +20,14 @@ except ImportError:
 class Var:
     # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", "13764765", cast=int)
     )
     API_HASH = (
         sys.argv[2]
         if len(sys.argv) > 2
-        else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        else config("API_HASH", "0255274ce6771d5535c201dd05ce8d79")
     )
-    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", "1BVtsOKcBu6vOM1tt4H059yWn-ILuAjUknCP9THYLkzgTg0GrDyDXzCGwa6gN6yCr8wStYimenOUmYLISlnb_NX2LkPzVtIQGxMxmjPxbjL9wmgbVBnHkXy1XKzWS5oxSuZvBy6S7_c7-NLKaWzC2AOquWF2zLbGAVz-37RfsjxsXyx5t2zHxBhAY9ncf3trjOILKt8u5dykPQtkq08vEaOApVYSx2n-PA3ILYlKVFTqPNB58YEr9YMPrcXM-2zeJgZNa4t_0N1U2MgtSmWZIXkxbex-eDZ8xmNad44WTUTqfo-di2kq8XktYzvr3Zfa7Rx0GJ0cFA-FYf0PQawHazhfH_fER8GU=")
     REDIS_URI = (
         sys.argv[4]
         if len(sys.argv) > 4
